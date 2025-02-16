03c2111ec52959c0afffb99467a8c5f340293a8c
NXPTUNX <135431914+nxptunx@users.noreply.github.com>
2025-02-16 13:22:52 +0400

    Add files via upload

--- samplex-as-module.py
+++ samplex-as-module.py
@@ -1,2 +1,10 @@
 import samplex
-samplex.download_samplefocus_mp3(input("Enter SampleFocus URL: ").strip())
+import samplexmore
+choose = input("Enter 1 for only one sample or 2 for more than 1 samples(space seperated): ")
+if choose == "1":
+    samplex.download_samplefocus_mp3(input("Enter SampleFocus URL: ").strip())
+elif choose == "2":
+    samplexmore.download_multiple(input("Enter SampleFocus URLs (space separated): ").strip().split(' '))
+else:
+    print("Invalid choice.")
+    
